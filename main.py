f28b1e195b98def44e9028ad2d5481a8acd21e8e
Oren3606 <oren.dev@icloud.com>
2024-04-28 15:38:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+import socket
+import argparse
+
+
+def encrypt():
+    pass
+
+
+def decrypt():
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="An encrypted console-based communication utility",
+                                     usage="secmsg [hash]")
+
+    # todo mode- receiver or sender
