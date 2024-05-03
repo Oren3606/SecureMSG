aa5c30a7d2004aa5e99f6ad56a2d6465e2fc58d7
Oren3606 <oren.dev@icloud.com>
2024-05-03 14:32:18 +0300

    experimenting

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 """
 import socket
-import argparse
+from secrets import token_hex
+from argparse import ArgumentParser
 
 
 def encrypt():
@@ -14,7 +15,42 @@ def decrypt():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="An encrypted console-based communication utility",
-                                     usage="secmsg [hash]")
+    print(token_hex()[:32])
+    parser = ArgumentParser(prog="Secure.MSG", usage="secmsg MODE [OPTIONS] NAME",
+                            description="An encrypted console-based communication utility",
+                            argument_default='--help'
+                            )
 
-    # todo mode- receiver or sender
+    parser.add_argument("mode", help="Mode of operation", type=str, choices=['connect', 'start'])
+    parser.add_argument("-name", help="Name to use for session", type=str, default="Anonymous", nargs=1)
+    parser.add_argument("-hash", help="Peer hash to connect to", type=str)
+
+    print(parser.parse_args())
+
+'''
+
+secmsg connect fehgerwg
+
+
+notes for self on usage examples-
+"" - given command
+>> - console output
+ 
+--------------------------------
+
+"secmsg connect h458e9ff name tom"
+>> now connecting...
+
+"secmsg listen name tom"
+>> hash generated:
+>> h458e9ff
+>> now listening for connections...
+>> new connection: tom
+
+"secmsg listen name tom hash=h458e9ff" 
+>> trying to use hash...
+>> success:
+>> h458e9ff
+>> now listening for connections...
+>> new connection: tom
+'''
